ee350e5d2b76d7c6723e803492c6d812bd412962
Eduardo Vila <eduardovila57@gmail.com>
2023-03-30 23:50:32 -0400

    no message

--- src/FinalProject.java
+++ src/FinalProject.java
@@ -3,6 +3,35 @@
     Eduardo Vila
  */
 
+import java.util.Scanner;
+
+abstract class Person {
+    private String name;
+    private String id;
+
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+    public String getId() {return id;}
+    public void setId(String id) {this.id = id;}
+}
+
+class Faculty extends Person{
+    private String rank;
+    //NEED TO INCLUDE LIST OF LECTURES TAUGHT BY THIS FACULTY (ARRAY OR INTERFACE? IDK YET)
+
+}
+
+class TA extends Person {
+    private String advisor;
+    private String degree;
+    //NEED TO INCLUDE LIST OF LABS SUPERVISED BY THIS TA (ARRAY OR INTERFACE? IDK YET)
+
+}
+
+class Student extends Person {
+    private String studentType;
+    //NEED TO INCLUDE POSSIBLE LIST OF LABS TAKEN BY THIS STUDENT (ARRAY OR INTERFACE? IDK YET)
+}
 
 public class FinalProject {
     public static void main(String[] args) {
