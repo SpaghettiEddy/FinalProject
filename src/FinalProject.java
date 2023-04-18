5129d6329453b3ff8d44f578a2e1dbdd3cc6d09d
Eduardo Vila <eduardovila57@gmail.com>
2023-04-18 16:36:43 -0400

    Working on menu option 1

--- src/FinalProject.java
+++ src/FinalProject.java
@@ -65,10 +65,10 @@ class Lab extends Course {
         setLocation(location);
     }
 
-    @Override
-    public String toString() {
-        return getCrn() + ',' + getLocation() + "\n";
-    }
+//    @Override
+//    public String toString() {
+//        return getCrn() + ',' + getLocation() + "\n";
+//    }
 }
 
 abstract class Person {
@@ -83,7 +83,6 @@ abstract class Person {
 
 class Faculty extends Person{
     private String rank;
-
 
 }
 
@@ -118,7 +117,8 @@ public class FinalProject {
     }
 
     public static void main(String[] args) {
-        Scanner myScan = new Scanner(System.in);
+
+        Scanner scanner = new Scanner(System.in);
         Scanner fileScan;
 
         String line;
@@ -126,11 +126,11 @@ public class FinalProject {
 
         ArrayList<Course> courseList = new ArrayList<Course>();
 
-        ArrayList<Faculty> schedule = new ArrayList<Faculty>();
+        ArrayList<Person> people = new ArrayList<Person>();
 
         System.out.print("Enter the absolute path of the file: ");
         do {                                                // Run until file is successfully loaded
-            String fileName = myScan.next();
+            String fileName = scanner.next();
             try {                                           // Attempts to take input from fileName
                 fileScan = new Scanner(new File(fileName)); // If an exception is caught at this line...
                 break;                                      // This line won't be reached (i.e. loop will never break)
@@ -140,21 +140,19 @@ public class FinalProject {
             }
         } while (true);
 
-        while(fileScan.hasNextLine()) {                     // Fill courseList with the courses in the lec.tct file
+        while (fileScan.hasNextLine()) {                     // Fill courseList with the courses in the lec.tct file
             line = fileScan.nextLine();
             arr = line.split(",");
             if (arr.length == 2) {  // If the array only has 2 elements, then it must be a lab
                 Lab temp = new Lab(Integer.parseInt(arr[0]), arr[1]);
                 courseList.add(temp);
-            }
-            else {                                  // If the array length is greater than 2, it must be a lecture
+            } else {                                  // If the array length is greater than 2, it must be a lecture
                 if (arr[4].equalsIgnoreCase("Online")) {    // We call the constructor for an online lecture
                     //public Lecture(int crn, String prefix, String title, String gradLevel, String modality) {  // Online lecture
                     Lecture temp = new Lecture(Integer.parseInt(arr[0]), arr[1], arr[2], arr[3], arr[4]);
                     courseList.add(temp);
-                }
-                else {      // We call the constructor for a lecture that has a physical location
-                    boolean hasLab = arr[6].equalsIgnoreCase("yes")?true:false;
+                } else {      // We call the constructor for a lecture that has a physical location
+                    boolean hasLab = arr[6].equalsIgnoreCase("yes") ? true : false;
                     Lecture temp = new Lecture(Integer.parseInt(arr[0]), arr[1], arr[2], arr[3], arr[4], arr[5], hasLab);
                     courseList.add(temp);
                 }
@@ -168,49 +166,80 @@ public class FinalProject {
         while (!option.equals("0")) {
             switch (option) {
                 case "1":
-                    Faculty temp = new Faculty();
-
-                    String tempID, tempName, tempRank, tempOffice;
-                    int numCourses;
-                    int lecturesTaught[];
-
-                    schedule.add(temp);
-                    System.out.print("Enter UCF id: ");
-                    tempID = myScan.next();
-                    myScan.nextLine();
-                    System.out.print("Enter name: ");
-                    tempName = myScan.nextLine();
-                    System.out.print("Enter rank: ");
-                    tempRank = myScan.next();
-                    System.out.print("Enter office location: ");
-                    tempOffice = myScan.next();
-                    System.out.print("Enter how many lectures: ");
-                    numCourses = myScan.nextInt();
-
-                    lecturesTaught = new int[numCourses];
-                    System.out.print("Enter the crns of the lectures: ");
-                    for (int i = 0; i < numCourses; i++)
-                        lecturesTaught[i] = myScan.nextInt();
-
+                    addFaculty(scanner, courseList, people);
                     System.out.println("DONE ~ TESTING");
 
                     break;
-                case "2":
-                    break;
-                case "3":
-                    break;
-                case "4":
-                    break;
-                case "5":
-                    break;
-                case "6":
-                    break;
-                case "7":
-                    break;
-                default:
-                    System.out.println("Invalid selection...");
+                    case "2":
+                        String tempStudent;
+                        System.out.print("Enter UCF id: ");
+                        tempStudent = scanner.next();
+                        System.out.println("Record found/Name: " + "ADD NAMESEARCH HERE");   // Have to search for name given ucfid
+                        //
+                        //  GOTTA FINISH
+                        //
+                        //
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        break;
+                    case "6":
+                        break;
+                    case "7":
+                        break;
+                    default:
+                        System.out.println("Invalid selection...");
+                }
+                option = menu();
             }
-            option = menu();
+
+        }
+
+    public static void addFaculty (Scanner scanner, ArrayList < Course > courseList, ArrayList < Person > people){
+        Faculty temp = new Faculty();
+
+        String tempId, tempName, tempRank, tempOffice;
+        int numCourses;
+        int lecturesTaught[];
+
+        people.add(temp);
+        System.out.print("Enter UCF id: ");
+        tempId = scanner.next();
+        scanner.nextLine();
+        System.out.print("Enter name: ");
+        tempName = scanner.nextLine();
+        System.out.print("Enter rank: ");
+        tempRank = scanner.next();
+        System.out.print("Enter office location: ");
+        tempOffice = scanner.next();
+        System.out.print("Enter how many lectures: ");
+        numCourses = scanner.nextInt();
+
+        lecturesTaught = new int[numCourses];
+        System.out.print("Enter the crns of the lectures: ");
+        for (int i = 0; i < numCourses; i++)
+            lecturesTaught[i] = scanner.nextInt();
+
+        for (int i = 0; i < numCourses; i++) {
+            for (int j = 0; j < courseList.size(); j++) {
+
+                if (courseList.get(j).getCrn() == lecturesTaught[i]) {
+                    if (((Lecture) courseList.get(j)).getModality().equalsIgnoreCase("Online")) //
+                        System.out.println("[" + courseList.get(j).getCrn() + "/" + ((Lecture) courseList.get(j)).getPrefix() +
+                                "/" + ((Lecture) courseList.get(j)).getTitle() + "]" + " Added!");
+                    else if (((Lecture) courseList.get(j)).isHasLab()) {
+                        System.out.println("[" + courseList.get(j).getCrn() + "/" + ((Lecture) courseList.get(j)).getPrefix() +
+                                "/" + ((Lecture) courseList.get(j)).getTitle() + "]" + " has these labs:");
+                        for (int k = 0; k < 3; k++) {
+                            System.out.println("\t\t\t" + courseList.get(j + k).getCrn() + "," + courseList.get(j + k).getLocation());
+                        }
+                    }
+                }
+            }
         }
     }
+
 }
